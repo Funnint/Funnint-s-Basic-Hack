d163e34d51aded1cb53a5a3d4c80fc08ac2bc5ef
Funnint <123704593+Funnint@users.noreply.github.com>
2024-04-14 22:17:46 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@
  Y888P  ~Y8888P' Y888888P      888888D      Y88888P ~Y8888P' YP   YP  CONVERTER
 ]=]
 
--- Instances: 4 | Scripts: 1 | Modules: 0
+-- Instances: 8 | Scripts: 1 | Modules: 0
 local G2L = {};
 
 -- StarterGui.ScreenGui
@@ -21,20 +21,54 @@ G2L["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
 G2L["2"] = Instance.new("LocalScript", G2L["1"]);
 
 
--- StarterGui.ScreenGui.Line
-G2L["3"] = Instance.new("ViewportFrame", G2L["1"]);
+-- StarterGui.ScreenGui.LocalScript.TextLabel
+G2L["3"] = Instance.new("TextLabel", G2L["2"]);
+G2L["3"]["TextWrapped"] = true;
+G2L["3"]["TextStrokeTransparency"] = 0.7300000190734863;
 G2L["3"]["BorderSizePixel"] = 0;
+G2L["3"]["TextScaled"] = true;
 G2L["3"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
-G2L["3"]["LightColor"] = Color3.fromRGB(255, 255, 255);
-G2L["3"]["Ambient"] = Color3.fromRGB(255, 255, 255);
-G2L["3"]["BackgroundTransparency"] = 1;
-G2L["3"]["Size"] = UDim2.new(1, 0, 1, 0);
+G2L["3"]["FontFace"] = Font.new([[rbxasset://fonts/families/Oswald.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["3"]["TextSize"] = 14;
+G2L["3"]["TextColor3"] = Color3.fromRGB(255, 229, 222);
+G2L["3"]["Size"] = UDim2.new(1, 0, 0.08404802531003952, 0);
 G2L["3"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
-G2L["3"]["Name"] = [[Line]];
+G2L["3"]["BackgroundTransparency"] = 1;
+G2L["3"]["Position"] = UDim2.new(0, 0, 0.7667238712310791, 0);
+
+-- StarterGui.ScreenGui.LocalScript.Spawn
+G2L["4"] = Instance.new("Sound", G2L["2"]);
+G2L["4"]["Name"] = [[Spawn]];
+G2L["4"]["SoundId"] = [[rbxassetid://2609981431]];
+
+-- StarterGui.ScreenGui.Line
+G2L["5"] = Instance.new("ViewportFrame", G2L["1"]);
+G2L["5"]["BorderSizePixel"] = 0;
+G2L["5"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["5"]["LightColor"] = Color3.fromRGB(255, 255, 255);
+G2L["5"]["Ambient"] = Color3.fromRGB(255, 255, 255);
+G2L["5"]["BackgroundTransparency"] = 1;
+G2L["5"]["Size"] = UDim2.new(1, 0, 1, 0);
+G2L["5"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["5"]["Name"] = [[Line]];
 
 -- StarterGui.ScreenGui.Line.WorldModel
-G2L["4"] = Instance.new("WorldModel", G2L["3"]);
+G2L["6"] = Instance.new("WorldModel", G2L["5"]);
 
+
+-- StarterGui.ScreenGui.Frame
+G2L["7"] = Instance.new("Frame", G2L["1"]);
+G2L["7"]["BorderSizePixel"] = 0;
+G2L["7"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["7"]["BackgroundTransparency"] = 1;
+G2L["7"]["Size"] = UDim2.new(1, 0, 0.838765025138855, 0);
+G2L["7"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+
+-- StarterGui.ScreenGui.Frame.UIListLayout
+G2L["8"] = Instance.new("UIListLayout", G2L["7"]);
+G2L["8"]["VerticalAlignment"] = Enum.VerticalAlignment.Bottom;
+G2L["8"]["HorizontalAlignment"] = Enum.HorizontalAlignment.Center;
+G2L["8"]["SortOrder"] = Enum.SortOrder.LayoutOrder;
 
 -- StarterGui.ScreenGui.LocalScript
 local function C_2()
@@ -159,30 +193,24 @@ local script = G2L["2"];
 		end
 	end)
 	
-	for i, Room:Model in pairs(CurrentRooms:GetChildren()) do
-		local doorModel = Room:WaitForChild("Door")
-		local highlight = highlightModel(doorModel:WaitForChild("Door"), Color3.fromRGB(255,255,255), nil, false)
-		doorModel:GetAttributeChangedSignal("Opened"):Connect(function()
-			highlight:StopHighlight()
-		end)
-	
-		if Room.Name == "50" then
+	local function notification(text, timea)
+		script.Spawn:Play()
+		local waititme = timea or 3
+		local clone = script.TextLabel:Clone()
+		clone.Text = text
+		clone.Parent = script.Parent.Frame
+		task.delay(waititme, function()
+			ts:Create(clone, TweenInfo.new(2, Enum.EasingStyle.Quint, Enum.EasingDirection.InOut), {TextTransparency = 1, TextStrokeTransparency = 1}):Play()
 			task.wait(2)
-		end
-	
-		for i, v:Model in pairs(Room:GetDescendants()) do
-			if v.Name == "KeyObtain" then
-				highlightModel(v, Color3.fromRGB(1,55,255))
-			elseif v.Name == "LiveHintBook" then
-				highlightModel(v, Color3.fromRGB(175, 223, 255))
-			elseif v.Name == "LeverForGate" then
-				highlightModel(v, Color3.fromRGB(252, 108, 42))
-			end
-		end
+			clone:Destroy()
+		end)
 	end
 	
-	
-	
+	workspace.ChildAdded:Connect(function(child)
+		if child.Name == "RushMoving" then
+			notification("Rush Spawned!")
+		end
+	end)
 end;
 task.spawn(C_2);
 
